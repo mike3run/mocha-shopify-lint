ac53377210c2487326d8cca04c9f2cf883f2801c
Miguel Palau <mpalauzarza@gmail.com>
2017-07-13 15:43:21 -0500

    more descriptions going on

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import globAll from 'glob-all'
 import { singleTest } from './lib'
 
+/**
+ * Runs theme-lint as mocha tests
+ *
+ * @param {Array} patterns - Globs are fine
+ * @returns {Promise} the result of linting a file
+ */
 const mochaShopifyLint = patterns => {
   describe('theme-lint', function () {
     globAll.sync(patterns)
