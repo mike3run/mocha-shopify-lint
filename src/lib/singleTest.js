92663fff32787f2c9778f652cf19d64297097ce7
Miguel Palau <mpalauzarza@gmail.com>
2017-07-13 00:28:51 -0500

    adding some comments to describe things

--- src/lib/singleTest.js
+++ src/lib/singleTest.js
@@ -1,17 +1,23 @@
 import { runAll } from 'theme-lint'
 import chalk from 'chalk'
 
+/**
+ * Test a file against theme-lint from shopify
+ * @param {String} path a single file location
+ */
 const singleTest = path => {
-  return runAll(path)
-    .then(reporter => {
-      reporter.finalize()
-    })
-    .catch(e => {
-      throw new Error(
-        chalk.white('Coded did not pass lint rules') +
-        chalk.red(e)
-      )
-    })
+  it(`${path} should pass theme-lint rules`, () => {
+    return runAll(path)
+      .then(reporter => {
+        reporter.finalize()
+      })
+      .catch(e => {
+        throw new Error(
+          chalk.white('Coded did not pass lint rules') +
+          chalk.red(e)
+        )
+      })
+  })
 }
 
 export default singleTest
